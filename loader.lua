0c90be2e7affbb2c5856c58787e2db2d02df3130
RainYuu <rainyuumail@pm.me>
2026-09-24 14:58:15 +0700

    Updated to latest version

--- loader.lua
+++ loader.lua
@@ -8,6 +8,16 @@ if getgenv()._RIXEL_HUB_LOADED then
     return
 end
 
-getgenv()._RIXEL_HUB_LOADED = true
 
-loadstring(game:HttpGet("https://jnkie.com/loaders/rixel/download"))()
+local content = game:HttpGet("https://jnkie.com/loaders/rixel")
+
+local loader_func, compile_err = loadstring(content)
+if not loader_func then
+    warn("[Rixel Hub Loader Error] Failed to load payload: " .. tostring(compile_err))
+    warn("[Debug Response] " .. tostring(content):sub(1, 200))
+    return
+end
+
+
+getgenv()._RIXEL_HUB_LOADED = true
+loader_func()
